d410c8ebcc3f4baf0c5671208e787a67ee8068ef
SimonPero <simonraulpero@gmail.com>
2026-05-15 17:36:00 -0300

    Corregido en base a las reglas generales

--- TpRecursion/catorce.java
+++ TpRecursion/catorce.java
@@ -8,18 +8,38 @@ package TpRecursion;
  */
 public class catorce {
     public static void mostrarSumaCols(int[][] matriz) {
-        recurMostrarSumaCols(matriz, 0, 0, 0);
+        recurMostrarCols(matriz, 0);
     }
 
-    public static void recurMostrarSumaCols(int[][] matriz, int i, int j, int suma) {
+    /**
+     * Esta funcion esta mal porque usa suma, que es lo que mostramos, como
+     * parametro
+     * public static void recurMostrarSumaCols(int[][] matriz, int i, int j, int
+     * suma) {
+     * if (j < matriz[0].length) {
+     * if (i < matriz.length) {
+     * recurMostrarSumaCols(matriz, i + 1, j, (suma + matriz[i][j]));
+     * } else {
+     * System.out.println("El resultado de sumar la columna " + j + " es de " +
+     * suma);
+     * recurMostrarSumaCols(matriz, 0, j + 1, 0);
+     * }
+     * }
+     * }
+     */
+    public static void recurMostrarCols(int[][] matriz, int j) {
         if (j < matriz[0].length) {
-            if (i < matriz.length) {
-                recurMostrarSumaCols(matriz, i + 1, j, (suma + matriz[i][j]));
-            } else {
-                System.out.println("El resultado de sumar la columna " + j + " es de " + suma);
-                recurMostrarSumaCols(matriz, 0, j + 1, 0);
-            }
+            int suma = recurSumaCols(matriz, 0, j);
+            System.out.println("El resultado de sumar la columna " + j + " es de " + suma);
+            recurMostrarCols(matriz, j + 1);
         }
+    }
+    public static int recurSumaCols(int[][] matriz, int i, int j) {
+        int suma = 0;
+        if (i < matriz.length) {
+            suma = matriz[i][j] + recurSumaCols(matriz, i + 1, j);
+        }
+        return suma;
     }
 
     public static void main(String[] args) {
